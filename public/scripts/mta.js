6df1ad221fa3e0a844098ad2a8e192acca221966
jhamadhav <jhamadhav28@gmail.com>
2023-02-10 05:19:07 +0530

    firebase redirect to github pages

--- public/scripts/mta.js
+++ public/scripts/mta.js
@@ -83,7 +83,7 @@ const googleSignIn = () => {
             console.log("sign in successful")
             console.log("logging into metamask");
             await connect()
-            window.location = "http://127.0.0.2:5500/index.html"
+            window.location = "https://jhamadhav.com/web3Campus/index.html"
         }).catch((error) = () => {
             console.log(error);
             console.log("sign in successful")
