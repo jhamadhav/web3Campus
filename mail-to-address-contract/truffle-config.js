f9811120c39c009a857a112325472a43ea2ee8aa
jhamadhav <jhamadhav28@gmail.com>
2023-02-09 14:27:58 +0530

    contract sample added

--- mail-to-address-contract/truffle-config.js
+++ mail-to-address-contract/truffle-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  contracts_build_directory: "./web3/contracts/",
+  contracts_build_directory: "./",
   networks: {
     development: {
       host: "127.0.0.1",     // Localhost (default: none)
