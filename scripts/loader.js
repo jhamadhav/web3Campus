1272c507eca1f3558d5530d878c59fe15b46cb6d
jhamadhav <jhamadhav28@gmail.com>
2023-02-09 21:42:28 +0530

    loader added

---
+++ scripts/loader.js
@@ -0,0 +1,6 @@
+const startLoad = () => {
+    document.getElementsByClassName("loader-wrapper")[0].style.display = "flex"
+}
+const endLoad = () => {
+    document.getElementsByClassName("loader-wrapper")[0].style.display = "none"
+}
--- scripts/main.js
+++ scripts/main.js
@@ -20,16 +20,19 @@ window.onload = async () => {
 
 const connect = async () => {
     if (letterProxy == null) return;
+    startLoad()
     await letterProxy.connect()
     console.log("on connect: account details");
     console.log(letterProxy.account);
 
     await mtaBridge.connect(letterProxy)
+    endLoad()
 }
 
 const create = async () => {
     // name, subject, description, recipients, institute, fileLink
     if (letterProxy == null) return;
+    startLoad()
     let name = document.getElementById("name-input").value;
     let subject = document.getElementById("subject-input").value;
     let description = document.getElementById("description-input").value;
@@ -46,15 +49,18 @@ const create = async () => {
     console.log(fileLink);
     let res = await letterProxy.createApplication(name, subject, description, recipients, institute, fileLink);
     console.log("on create: ");
-    // console.log(res);
+    console.log(res);
+    endLoad()
 }
 
 const getAppByInstitute = async () => {
     if (letterProxy == null) return;
+    startLoad()
     let institute = document.getElementById("by-institute-input").value;
     let res = await letterProxy.getApplicationByInstitute(institute);
     console.log(`get application by institute: ${institute} `);
     console.log(res);
+    endLoad()
 }
 
 const getAddressFromMail = async (mail) => {
